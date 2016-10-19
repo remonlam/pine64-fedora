02a02ab68f0869c4e2ee44b9aadf3832353374bf
Remon Lam <remon.lam@lamnet.nl>
2016-10-19 10:38:29 +0200

    push

---
+++ testing.sh
@@ -0,0 +1,8 @@
+curl -sSL -o CentOS-7-aarch64-rootfs-1606.tar.xz http://mirror.centos.org/altarch/7/isos/aarch64/CentOS-7-aarch64-rootfs-1606.tar.xz
+
+
+UUID="424A-49A2" TYPE="vfat" /boot  vfat  defaults 0 0
+UUID="17aef2e3-2c9f-4199-afb8-41ec63c92849" / ext4  defaults  0 0
+
+
+available
